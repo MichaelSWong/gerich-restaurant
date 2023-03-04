9cf967e783559aaa8f5a3384035cee9d3a745480
Michael Wong <mizik_jah@yahoo.com>
2023-03-04 10:46:33 -0500

    updated menuitem and specialmenu components

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { FC } from 'react';
+import './MenuItem.css';
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+type MenuItemProps = {
+  title: string;
+  tags: string;
+  price: string;
 };
+
+const MenuItem: FC<MenuItemProps> = ({ title, price, tags }) => (
+  <div className='app__menuitem'>
+    <div className='app__menuitem-head'>
+      <div className='app__menuitem-name'>
+        <p className='p__cormorant' style={{ color: '#DCCA87' }}>
+          {title}
+        </p>
+      </div>
+      <div className='app__menuitem-dash' />
+      <div className='app__menuitem-price'>
+        <p className='p__cormorant'>{price}</p>
+      </div>
+    </div>
+
+    <div className='app__menuitem-sub'>
+      <p className='p__opensans' style={{ color: '#AAAAAA' }}>
+        {tags}
+      </p>
+    </div>
+  </div>
+);
 
 export default MenuItem;
