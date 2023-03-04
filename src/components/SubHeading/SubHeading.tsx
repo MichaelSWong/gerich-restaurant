0d98a242dc2a6c4fbac6e74ce7f71d82ba70f2db
Michael Wong <mizik_jah@yahoo.com>
2023-03-04 08:02:37 -0500

    created reusable SubHeading comp, added Header

--- src/components/SubHeading/SubHeading.tsx
+++ src/components/SubHeading/SubHeading.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => {
-  return <div>SubHeading</div>;
+type SubHeadingProps = {
+  title: string;
+};
+
+const SubHeading: FC<SubHeadingProps> = ({ title }) => {
+  return (
+    <div style={{ marginBottom: '1rem' }}>
+      <p className='p__cormorant'>{title}</p>
+      <img src={images.spoon} alt='spoon' className='spoon__img' />
+    </div>
+  );
 };
 
 export default SubHeading;
